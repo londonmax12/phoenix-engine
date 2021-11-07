88f3e42c0fa22314e56ca956b391bcf33cf4c3b4
Mercury <londonhmball@gmail.com>
2021-11-07 21:21:33 +1100

    Added icons to content browser

--- Phoenix-Editor/src/Panels/ContentBrowserPanel.cpp
+++ Phoenix-Editor/src/Panels/ContentBrowserPanel.cpp
@@ -6,44 +6,115 @@
 namespace phx {
 	static const std::filesystem::path s_AssetPath = "assets";
 
+	static float padding = 16.0f;
+	static float thumbnailSize = 100.0f;
+	float cellSize = thumbnailSize + padding;
+
+	void ContentBrowserPanel::Refresh()
+	{
+		m_Files.clear();
+
+		for (auto& itr : std::filesystem::directory_iterator(m_CurrentDirectory))
+		{
+			FileIcon newFileIcon;
+			newFileIcon.Path = itr;
+			
+			m_Files.push_back(newFileIcon);
+		}
+	}
+
 	ContentBrowserPanel::ContentBrowserPanel()
 		: m_CurrentDirectory(s_AssetPath)
 	{
+		m_DirectoryEmptyIcon = Texture2D::Create("resources/icons/content-browser/directory-empty-icon.png");
+		m_DirectoryIcon = Texture2D::Create("resources/icons/content-browser/directory-icon.png");
+		m_FileIcon = Texture2D::Create("resources/icons/content-browser/file-icon.png");
+		m_ImageIcon = Texture2D::Create("resources/icons/content-browser/image-icon.png");
 
+		Refresh();
 	}
 
 	void ContentBrowserPanel::OnImGuiRender()
 	{
 		ImGui::Begin("Content Browser");
 
-		if (m_CurrentDirectory != std::filesystem::path(s_AssetPath))
+		if (ImGui::Button("<-"))
 		{
-			if (ImGui::Button("<-"))
+			if (m_CurrentDirectory != std::filesystem::path(s_AssetPath))
 			{
 				m_CurrentDirectory = m_CurrentDirectory.parent_path();
-			}
+			}		
 		}
-
-		for (auto& itr : std::filesystem::directory_iterator(m_CurrentDirectory))
+		ImGui::SameLine();
+		if (ImGui::Button("+"))
 		{
-			const auto& path = itr.path();
-			auto relativePath = std::filesystem::relative(itr.path(), s_AssetPath);
+			std::string newDir = std::string(std::filesystem::absolute(m_CurrentDirectory).string() + "\\New Folder");
+			mkdir(newDir.c_str());
+		}
+		ImGui::Separator();
+
+		float panelWidth = ImGui::GetContentRegionAvail().x;
+		int columnCount = (int)(panelWidth / cellSize);
+		if (columnCount < 1)
+			columnCount = 1;
+
+		ImGui::Columns(columnCount, 0, false);
+
+
+		for (auto& itr : m_Files)
+		{
+			const auto& path = itr;
+			auto relativePath = std::filesystem::relative(itr.Path, s_AssetPath);
 			std::string filenameString = relativePath.filename().string();
 
-			if (itr.is_directory())
+			Ref<Texture2D> icon;
+
+			if (std::filesystem::is_directory(itr.Path))
 			{
-				if (ImGui::Button(filenameString.c_str()))
+				if (std::filesystem::is_empty(itr.Path))
 				{
-					m_CurrentDirectory /= itr.path().filename();
+					icon = m_DirectoryEmptyIcon;
+				}
+				else
+				{
+					icon = m_DirectoryIcon;
 				}
 			}
 			else
 			{
-				if (ImGui::Button(filenameString.c_str()))
+				std::string extension = itr.Path.extension().string();
+				if (extension == ".png" || extension == ".jpg")
 				{
+					icon = m_ImageIcon;
+				}
+				else
+				{
+					icon = m_FileIcon;
+				}
+				
+			}
+			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.0f, 0.0f, 0.0f, 0.0f));
+			ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
+			ImGui::PopStyleColor();
+
+			if (ImGui::IsItemHovered())
+			{
+				if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+				{
+					if (std::filesystem::is_directory(itr.Path))
+					{
+						m_CurrentDirectory /= itr.Path.filename();
+					}
 				}
 			}
+
+			ImGui::TextWrapped(filenameString.c_str());
+
+			ImGui::NextColumn();
 		}
+
+
+		ImGui::Columns(1);
 
 		ImGui::End();
 	}
--- Phoenix-Editor/src/Panels/ContentBrowserPanel.h
+++ Phoenix-Editor/src/Panels/ContentBrowserPanel.h
@@ -2,13 +2,32 @@
 
 #include <filesystem>
 
+#include "Phoenix/Renderer/Texture.h"
+
 namespace phx {
+	struct FileIcon
+	{
+		FileIcon() = default;
+		std::filesystem::path Path;
+		Ref<Texture2D> icon;
+
+		operator std::filesystem::path() const { return Path; }
+	};
+
 	class ContentBrowserPanel
 	{
-	public:
+	public:		
 		ContentBrowserPanel();
-		void OnImGuiRender();
+		void Refresh();
+		void OnImGuiRender();	
 	private:
 		std::filesystem::path m_CurrentDirectory;
+
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_DirectoryEmptyIcon;
+		Ref<Texture2D> m_ImageIcon;
+		Ref<Texture2D> m_FileIcon;
+
+		std::vector<FileIcon> m_Files;
 	};
 }
